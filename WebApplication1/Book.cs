3a1373da1843dbc92e214c8b8a0d8f7e8b3411ef
Martin070106 <kameczmartin@gmail.com>
2025-10-03 10:36:21 +0200

    bodi help

---
+++ WebApplication1/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Aurhor { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+
+    }
+}
---
+++ WebApplication1/Connect.cs
@@ -0,0 +1,29 @@
+﻿namespace WebApplication1
+{
+    public class Connect
+    {
+        public MySqlConnection? Connection;
+
+        private string _host;
+        private string _db;
+        private string _user;
+        private string _password;
+
+        private string _ConnectionString;
+
+
+        public Connect()
+        {
+            _host = "localhost";
+            _db = "library";
+            _user = "root";
+            _password = " ";
+
+            _ConnectionString = $"SERVER={_host}; DATABASE={_db};UID={_user}; PASSWORD={_password};SslMode=none";
+
+            Connection = new MySqlConnection(_ConnectionString);
+
+
+        }
+    }
+}
